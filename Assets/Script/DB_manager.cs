acd7b5f4359ee1c0675f42e1a2aaea0d706658aa
hamsa45 <abhilashabhi079@gmail.com>
2023-10-06 11:01:07 +0530

    firebase app

--- Assets/Script/DB_manager.cs
+++ Assets/Script/DB_manager.cs
@@ -18,16 +18,39 @@ public class DB_manager : MonoBehaviour
     private float expStartTime;
 
     private DatabaseReference dbRef;
+    FirebaseApp app;
     private FirebaseDatabase database;
 
     // Start is called before the first frame update
     void Start()
-    {
+	{
+        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => 
+        //{
+        //    if (task.IsFaulted && task.Exception != null)
+		//	{
+        //        Debug.Log(" encountered fault ");
+		//	}
+        //    else if(task.IsCompleted)
+		//	{
+        //        app = FirebaseApp.DefaultInstance;
+        //        app.Options.DatabaseUrl = new Uri("https://fir-databasetest2-e7e86-default-rtdb.firebaseio.com/");
+        //        //FirebaseApp.DefaultInstance.Options.DatabaseUrl = new Uri("https://fir-databasetest2-e7e86-default-rtdb.firebaseio.com/");
+        //    }
+        //});
+        StartCoroutine(databaseInstaceInit());   
+    }
+
+	IEnumerator databaseInstaceInit()
+	{
+        yield return new WaitForSeconds(1f);
+        Debug.Log(" database instance getting ++++");
+        //database = FirebaseDatabase.DefaultInstance;
         database = FirebaseDatabase.GetInstance("https://fir-databasetest2-e7e86-default-rtdb.firebaseio.com/");
         dbRef = database.RootReference;
+        startExperience();
     }
-    
-    public void updateUser()
+
+	public void updateUser()
 	{
         ////User newUser = new User(_nameInput.text, _time, int.Parse(_count.text));
         //TimeSpan time2 = TimeSpan.FromSeconds(120f);
@@ -35,19 +58,15 @@ public class DB_manager : MonoBehaviour
         //string jsonString = JsonUtility.ToJson(newUser);
 
         ////dbRef.Child(headID).Child(Exp).SetRawJsonValueAsync(jsonString);
-        //dbRef.Child(Exp).SetRawJsonValueAsync(jsonString);
-        DatabaseReference expRef = dbRef.Child(Exp);
-        getValue();
+        ////dbRef.Child(Exp).SetRawJsonValueAsync(jsonString);
+        //DatabaseReference expRef = dbRef.Child(Exp);
+        
+        //getValue();
 	}
 
-    public void startExperience(string ExpName)
+    private void startExperience()
 	{
         expStartTime = Time.time;
-	}
-
-    public void endExperience(string ExpName)
-	{
-
 	}
 
     private void getValue()
@@ -67,10 +86,54 @@ public class DB_manager : MonoBehaviour
         });
     }
 
-    public void updateValue(string ExpName, string time)
+    public void checkAndSetExperienceUseTime(String ExpName)
+    {
+        //database.GetReference(headID).Child(Exp).Child("ExpName").GetValueAsync().ContinueWithOnMainThread(task => 
+        database.GetReference(Exp).GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            Debug.Log($" {ExpName} is the name provided and task completed");
+            if (ExpName.Length == 0) ExpName = _nameInput.text;
+            //if (ExpName == null)
+            //{
+            //    Debug.Log("experince name field is Empty");
+            //    return;
+            //}
+
+            ExpName = ExpName.ToLower();
+
+            float expUseTime = Time.time - expStartTime;
+            TimeSpan totalTimeSpan = TimeSpan.FromSeconds(expUseTime);
+            expStartTime = Time.time;
+            if (task.IsFaulted)
+            {
+                Debug.Log("error while getting reference of data");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                if (snapshot.HasChild(ExpName))
+				{
+                    string timeSpanValue = (string) snapshot.Child(ExpName).Value;
+                    Debug.Log($" ++++ previous time of {ExpName} : {timeSpanValue}");
+                    TimeSpan netTimeSpan = totalTimeSpan.Add(TimeSpan.Parse(timeSpanValue));
+                    Debug.Log($" ++++ total net time of {ExpName}: {totalTimeSpan} + {timeSpanValue} = {netTimeSpan}");
+                    database.GetReference(Exp).Child(ExpName).SetValueAsync(netTimeSpan.ToString());
+                }
+                else
+				{
+                    database.GetReference(Exp).Child(ExpName).SetValueAsync(totalTimeSpan.ToString());
+                }
+            }
+        });
+    }
+}
+
+public class senseExp
+{
+    public string ExpUseTime;
+
+    public senseExp(string time)
 	{
-
+        this.ExpUseTime = time;
 	}
-
-
 }
