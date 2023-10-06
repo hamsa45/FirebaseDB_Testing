2676c646ed831d0b76ce2ae6f40334bdb4713fc9
hamsa45 <abhilashabhi079@gmail.com>
2023-10-06 11:14:36 +0530

    modified for database only

--- Assets/Script/DB_manager.cs
+++ Assets/Script/DB_manager.cs
@@ -24,81 +24,39 @@ public class DB_manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
 	{
-        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => 
-        //{
-        //    if (task.IsFaulted && task.Exception != null)
-		//	{
-        //        Debug.Log(" encountered fault ");
-		//	}
-        //    else if(task.IsCompleted)
-		//	{
-        //        app = FirebaseApp.DefaultInstance;
-        //        app.Options.DatabaseUrl = new Uri("https://fir-databasetest2-e7e86-default-rtdb.firebaseio.com/");
-        //        //FirebaseApp.DefaultInstance.Options.DatabaseUrl = new Uri("https://fir-databasetest2-e7e86-default-rtdb.firebaseio.com/");
-        //    }
-        //});
-        StartCoroutine(databaseInstaceInit());   
-    }
-
-	IEnumerator databaseInstaceInit()
-	{
-        yield return new WaitForSeconds(1f);
-        Debug.Log(" database instance getting ++++");
-        //database = FirebaseDatabase.DefaultInstance;
         database = FirebaseDatabase.GetInstance("https://fir-databasetest2-e7e86-default-rtdb.firebaseio.com/");
         dbRef = database.RootReference;
         startExperience();
     }
-
-	public void updateUser()
-	{
-        ////User newUser = new User(_nameInput.text, _time, int.Parse(_count.text));
-        //TimeSpan time2 = TimeSpan.FromSeconds(120f);
-        //User2 newUser = new User2(_nameInput.text, time2.ToString());
-        //string jsonString = JsonUtility.ToJson(newUser);
-
-        ////dbRef.Child(headID).Child(Exp).SetRawJsonValueAsync(jsonString);
-        ////dbRef.Child(Exp).SetRawJsonValueAsync(jsonString);
-        //DatabaseReference expRef = dbRef.Child(Exp);
-        
-        //getValue();
-	}
 
     private void startExperience()
 	{
         expStartTime = Time.time;
 	}
 
-    private void getValue()
-	{
-        //database.GetReference(headID).Child(Exp).Child("ExpName").GetValueAsync().ContinueWithOnMainThread(task => 
-        database.GetReference(Exp).GetValueAsync().ContinueWithOnMainThread(task => 
-        {
-           if (task.IsFaulted)
-           {
-                Debug.Log("error while getting reference of data");
-           }
-           else if (task.IsCompleted)
-           {
-               DataSnapshot snapshot = task.Result;
-               _count.text = (string) snapshot.Value;
-           }
-        });
-    }
+    //private void getValue()
+	//{
+    //    //database.GetReference(headID).Child(Exp).Child("ExpName").GetValueAsync().ContinueWithOnMainThread(task => 
+    //    database.GetReference(Exp).GetValueAsync().ContinueWithOnMainThread(task => 
+    //    {
+    //       if (task.IsFaulted)
+    //       {
+    //            Debug.Log("error while getting reference of data");
+    //       }
+    //       else if (task.IsCompleted)
+    //       {
+    //           DataSnapshot snapshot = task.Result;
+    //           _count.text = (string) snapshot.Value;
+    //       }
+    //    });
+    //}
 
     public void checkAndSetExperienceUseTime(String ExpName)
     {
-        //database.GetReference(headID).Child(Exp).Child("ExpName").GetValueAsync().ContinueWithOnMainThread(task => 
         database.GetReference(Exp).GetValueAsync().ContinueWithOnMainThread(task =>
         {
             Debug.Log($" {ExpName} is the name provided and task completed");
             if (ExpName.Length == 0) ExpName = _nameInput.text;
-            //if (ExpName == null)
-            //{
-            //    Debug.Log("experince name field is Empty");
-            //    return;
-            //}
-
             ExpName = ExpName.ToLower();
 
             float expUseTime = Time.time - expStartTime;
